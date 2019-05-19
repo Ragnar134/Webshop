66e8fe89d825ec931e8dfe850377eeb42c609746
Georgi Ivanov <pandoraast389@outlook.com>
2019-05-20 01:33:24 +0200

    Commit Juist

--- Webshop/WinkelmandjeLeeg.aspx.cs
+++ Webshop/WinkelmandjeLeeg.aspx.cs
@@ -10,10 +10,15 @@ namespace Webshop
 {
     public partial class WinkelmandjeLeeg : System.Web.UI.Page
     {
+        /* Dit is een instance van de Controller Class zodat we in deze document gebruik kunnen maken 
+          van de methodes die in de Controller Class zitten */
 
         Controller _cont = new Controller();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            /* Alle gegevens van de klant ophalen en vertonen!*/
+
             lblKlantNr.Text = _cont.laadKlant(Convert.ToInt32(Context.User.Identity.Name)).KlantNr.ToString();
             lblNaam.Text = _cont.laadKlant(Convert.ToInt32(Context.User.Identity.Name)).Voornaam + " " + _cont.laadKlant(Convert.ToInt32(Context.User.Identity.Name)).Naam;
             lblAdres.Text = _cont.laadKlant(Convert.ToInt32(Context.User.Identity.Name)).Adres.ToString();
@@ -21,6 +26,8 @@ namespace Webshop
             lblGemeente.Text = _cont.laadKlant(Convert.ToInt32(Context.User.Identity.Name)).Gemeente.ToString();
             lblDatum.Text = DateTime.Now.ToLongDateString();
         }
+
+        /* Bij het klikken van deze knop wordt de gebruiker terug naar de catalogus doorverwezen.*/
 
         protected void btnTerug_Click(object sender, EventArgs e)
         {
--- Webshop/bestelbevestiging.aspx.cs
+++ Webshop/bestelbevestiging.aspx.cs
@@ -9,6 +9,9 @@ namespace Webshop
 {
     public partial class bestelbevestiging : System.Web.UI.Page
     {
+        /*De tekst voor de bestelbevestiging hebben we vastgecodeerd in bestelbevestiging.aspx
+       Hier halen we de Sessie variabelen voor de ordernummer en de totaal prijs.*/
+
         protected void Page_Load(object sender, EventArgs e)
         {
             lblOrderNr.Text = Convert.ToInt32(Session["ordernr"]).ToString();
@@ -16,6 +19,7 @@ namespace Webshop
 
         }
 
+        /* Bij het klikken van deze knop wordt de gebruiker terug naar de catalogus doorverwezen.*/
         protected void btnTerugNaarCath_Click(object sender, EventArgs e)
         {
             Response.Redirect("default.aspx");
